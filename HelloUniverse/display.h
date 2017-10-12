cb21e159beb41f53e60fe9570eac1738096e4469
Maxwell Pray <synthead@github.com>
2017-10-12 16:45:59 -0700

    Move display characters to a struct.

--- HelloUniverse/display.h
+++ HelloUniverse/display.h
@@ -1,7 +1,17 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
+#include <Arduino.h>
+
 namespace Display {
+  struct display_chars_t {
+    byte top_left[8];
+    byte top_right[8];
+    byte bottom_left_1[8];
+    byte bottom_left_2[8];
+    byte bottom_right[8];
+  };
+
   void setup();
   void print_all();
   void print_wave_1();
